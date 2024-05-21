270de9e6df773987ab60aae0589a483ab1716dec
David Walters <154628764+Dwalters10@users.noreply.github.com>
2024-05-21 03:40:44 -0600

    Add files via upload

---
+++ Branching Assignment/Branching Assignment/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // opening text
+            Console.WriteLine("Welcome to Package Express.");
+            Console.WriteLine("Please follow the instructions below.");
+            // get weight, height, length and width 
+            Console.WriteLine("Enter package weight.");
+            int Num1 = 0;
+            int Num2 = 0;
+            int Num3 = 0;
+            int total = 0;
+            decimal total1 = 0;
+
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if (weight > 50)
+            // Too heavy loop
+            {
+                Console.WriteLine("Your package is too heavy to be shipped by Package Express. Thankyou.");
+                
+
+
+            }
+            
+            else
+            {
+
+                Console.WriteLine("Please enter package width.");
+                Num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter package length.");
+                Num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pleae enter package height.");
+                Num3 = Convert.ToInt32(Console.ReadLine());
+                // total size calculation
+                total = (Num1 + Num2 + Num3);
+                // price calculation
+                total1 = (Num1 * Num2 * Num3 * weight / 100);
+            }
+
+
+            if (total > 50)
+            //    // too big loop
+            {
+                Console.WriteLine("Package exceeds sipping size for Package Express. Thankyou.");
+                Console.ReadLine();
+            }
+
+            // price quote loop
+            else if (weight < 50 && total < 50) 
+            {
+                Console.WriteLine("Your shipping quote is " + (total1));
+                Console.ReadLine();
+            }
+            else
+            { Console.ReadLine(); }
+        }
+    }
+}
---
+++ Console App Assignment/Console App Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is a do while loop
+            int count = 0;
+            do
+            {
+                Console.WriteLine("Inside the loop");
+                count++; // add one to count each time 
+            }
+            while (count < 5); // continue until you reach 5
+
+            // this is a while loop
+            int counting = 0;
+            while (counting < 3) // and the counting will be 3
+            {
+                Console.WriteLine("Counting numbers");
+                counting++; // add one to counting each time
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
